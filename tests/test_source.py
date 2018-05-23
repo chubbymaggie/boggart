28a85f6d93b6626f55bca00135e78179cb4d2b08
ChrisTimperley <christimperley@googlemail.com>
2018-05-23 18:19:53 -0400

    added MockSourceFileManager and added additional cases to test_line_col_to_offset

--- tests/test_source.py
+++ tests/test_source.py
@@ -8,6 +8,12 @@ from boggart.config.operators import Operators as OperatorManager
 from boggart.server.sourcefile import SourceFileManager
 
 
+class MockSourceFileManager(SourceFileManager):
+    def __init__(self, src: str) -> None:
+        super().__init__(None, None, OperatorManager())
+        self.read_file = MagicMock(return_value=src)
+
+
 class MockSnapshot(object):
     def name(self) -> str:
         return "foo"
@@ -16,8 +22,7 @@ class MockSnapshot(object):
 def test_line_col_to_offset():
     def build_convert(src: str) -> Callable[[int, int], int]:
         snapshot = MockSnapshot()
-        mgr = SourceFileManager(None, None, OperatorManager())
-        mgr.read_file = MagicMock(return_value=src)
+        mgr = MockSourceFileManager(src)
         def convert(line: int, col: int) -> int:
             return mgr.line_col_to_offset(snapshot, "foo.c", line, col)
         return convert
@@ -31,3 +36,5 @@ for (int i = 0; i < 10; ++i) {
 
     assert convert(1, 0) == 0
     assert convert(1, 11) == 11
+    assert convert(2, 0) == 12
+    assert convert(2, 5) == 17
