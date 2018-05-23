4b5f13965c8e862ebe629c362a99b321603a0a07
ChrisTimperley <christimperley@googlemail.com>
2018-05-23 18:03:45 -0400

    added test case for line_col_to_offset

---
+++ tests/test_source.py
@@ -0,0 +1,33 @@
+from typing import Callable
+from unittest.mock import MagicMock
+
+import pytest
+from bugzoo.core.bug import Bug as Snapshot
+
+from boggart.config.operators import Operators as OperatorManager
+from boggart.server.sourcefile import SourceFileManager
+
+
+class MockSnapshot(object):
+    def name(self) -> str:
+        return "foo"
+
+
+def test_line_col_to_offset():
+    def build_convert(src: str) -> Callable[[int, int], int]:
+        snapshot = MockSnapshot()
+        mgr = SourceFileManager(None, None, OperatorManager())
+        mgr.read_file = MagicMock(return_value=src)
+        def convert(line: int, col: int) -> int:
+            return mgr.line_col_to_offset(snapshot, "foo.c", line, col)
+        return convert
+
+    convert = build_convert("""
+int sm = 0;
+for (int i = 0; i < 10; ++i) {
+  sm += i;
+}
+    """.strip())
+
+    assert convert(1, 0) == 0
+    assert convert(1, 11) == 11
